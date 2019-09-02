067d93572c657bea10416de99dc7d4a123db9475
bxker <jake.luke.baker@gmail.com>
2019-09-01 19:20:41 -0500

    cleaned up some console logs

--- server/controllers/partsController.js
+++ server/controllers/partsController.js
@@ -5,16 +5,14 @@ let parts = [
         part_name: "Case",
         description: "InWin 303 Black ATX Mid Tower Computer Case with Tempered Glass, Black",
         link: "https://www.amazon.com/InWin-303-Black-Computer-Tempered/dp/B01GQNOTEC",
-        image: "https://images-na.ssl-images-amazon.com/images/I/617%2B3Z-Vu%2BL._SL1117_.jpg",
-        display: "parts"
+        image: "https://images-na.ssl-images-amazon.com/images/I/617%2B3Z-Vu%2BL._SL1117_.jpg"
 
     },
     {
         part_name: "CPU",
         description: "Intel Core i9-9900K Desktop Processor 8 Cores up to 5.0 GHz Turbo unlocked LGA1151 300 Series 95W",
         link: "https://www.amazon.com/Intel-i9-9900K-Desktop-Processor-Unlocked/dp/B005404P9I/ref=sr_1_2?crid=YXRYG3FLG9HX&keywords=i7+9900k&qid=1567033437&s=electronics&sprefix=i7%2Celectronics%2C152&sr=1-2",
-        image: "https://images-na.ssl-images-amazon.com/images/I/71RsweT83eL._SL1500_.jpg",
-        display: "parts"
+        image: "https://images-na.ssl-images-amazon.com/images/I/71RsweT83eL._SL1500_.jpg"
 
     }
 ];
@@ -26,7 +24,6 @@ const getParts = (req, res) => {
 
 const addPart = (req, res) => {
     parts.push(req.body)
-    // purchasesController.purchased.push(req.body)
     res.json(parts)
 }
 
@@ -36,12 +33,8 @@ const deletePart = (req, res) => {
 }
 
 const movePart = (req, res) => {
-    // console.log('hit')
     let item = parts.splice(+req.params.id, 1)
-    // console.log(item)
     purchasesController.purchased.push(item[0])
-    // console.log(parts)
-    // console.log(purchasesController.purchased)
     res.json(parts)
 }
 
--- server/controllers/purchasesController.js
+++ server/controllers/purchasesController.js
@@ -3,8 +3,7 @@ let purchased = [
         part_name: "Motherboard",
         description: "ASUS ROG Maximus XI Hero (Wi-Fi) Z390 Gaming Motherboard LGA1151 (Intel 8th 9th Gen) ATX DDR4 DP HDMI M.2 USB 3.1 Gen2 802.11AC Wi-Fi",
         link: "https://www.amazon.com/ROG-Maximus-Hero-Wi-Fi-Motherboard/dp/B07HCXT591/ref=sr_1_1?keywords=motherboard+for+9900k&qid=1567094110&s=electronics&sr=1-1",
-        image: "https://images-na.ssl-images-amazon.com/images/I/81Du2vIqOmL._SL1500_.jpg",
-        display: "purchased"
+        image: "https://images-na.ssl-images-amazon.com/images/I/81Du2vIqOmL._SL1500_.jpg"
     }
 ];
 
